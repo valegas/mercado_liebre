fa3cc65ff63eb4724a207fdc91877691f85a7544
gaston <gastoncastillo1920@gmail.com>
2024-01-25 01:29:09 -0300

    estaba haciendo arreglos html ,app.js y en css

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ app.use(express.static('public'))
 app.get("/", (req,res)=>{
     res.sendFile(path.join(__dirname,"/views/home.html"))
 })
+app.get("/registro", (req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/register.html"))
+})
+app.get("/login", (req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/login.html"))
+})
+
+
 
 
 
